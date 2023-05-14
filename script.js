fe6cb92b41571b8fd7ac09f0d991f0c4c1367f0c
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-05-14 00:32:10 -0400

    adds next eventListener

--- script.js
+++ script.js
@@ -20,3 +20,31 @@ for (let i = 0; i < cells.length; i++) {
 //cells.forEach(function(cell) {
 //    cell.addEventListener("click", function() {
 //        console.log("Cell clicked: " + cell.innerHTML);
+
+
+fetch("http://localhost:3000/Pizza")
+.then(response => response.json())
+.then(data => {
+    //console.log(data)
+    //const pizzaData = data;
+    //console.log(pizzaData)
+    const tableRows = document.querySelectorAll("tbody tr");
+    const detailsContainer = document.getElementById("pizza-details");
+
+    tableRows.forEach(row => {
+      row.addEventListener("click", () => {
+        const pizzaId = parseInt(row.dataset.pizzaId);
+        
+        const pizza = data.find(pizza => pizza.id === pizzaId);
+    
+
+        if (pizza) {
+          detailsContainer.innerHTML = `
+            <h2>${pizza.name}</h2>
+            <p><strong>Toppings:</strong> ${pizza.toppings.join(", ")}</p>
+            <p><strong>Recipe:</strong> ${pizza.recipes}</p>
+          `;
+        }
+      });
+    });
+  })
