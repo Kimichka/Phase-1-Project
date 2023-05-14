bb3db0a03f862a2668a2309f69a0e722cf663f8a
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-05-13 23:35:41 -0400

    Adds event Listener

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 console.log("script.js is running")
 
+let cells = document.getElementsByTagName("td");
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("mouseover", function() {
+    this.style.backgroundColor = "red";
+  });
 
-fetch("http://localhost:3000/pizza")
-.then(response => response.json())
-.then(data => console.log(data))
+  cells[i].addEventListener("mouseout", function() {
+    this.style.backgroundColor = "";
+  });
+}
+
+//fetch("http://localhost:3000/pizza")
+//.then(response => response.json())
+//.then(data => console.log(data))
 
 
-let cells = document.querySelectorAll("#myTable tbody tr td");
-cells.forEach(function(cell) {
-    cell.addEventListener("click", function() {
-        console.log("Cell clicked: " + cell.innerHTML);
-    });
-});
+//let cells = document.querySelectorAll("#myTable tbody tr td");
+//cells.forEach(function(cell) {
+//    cell.addEventListener("click", function() {
+//        console.log("Cell clicked: " + cell.innerHTML);
+
