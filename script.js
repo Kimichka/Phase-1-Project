d48bfc55fa72c518500e7c51597d85e96da74e4f
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-05-13 22:13:30 -0400

    Adds first eventListener

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ console.log("script.js is running")
 fetch("http://localhost:3000/pizza")
 .then(response => response.json())
 .then(data => console.log(data))
+
+
+let cells = document.querySelectorAll("#myTable tbody tr td");
+cells.forEach(function(cell) {
+    cell.addEventListener("click", function() {
+        console.log("Cell clicked: " + cell.innerHTML);
+    });
+});
